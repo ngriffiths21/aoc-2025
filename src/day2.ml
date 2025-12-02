4aa2dcc37b6be3d1f66261a8bcc7d93723ef4bd9
Nicholas Griffiths <ngriffiths21@gmail.com>
2025-12-02 11:59:03 -0500

    day 2, part 2

--- src/day2.ml
+++ src/day2.ml
@@ -20,14 +20,24 @@ let repeat num digits = num * (Int.pow 10 digits) + num
 let%test_unit "repeat digits" =
   [%test_eq:int] (repeat 123 3) 123123
 
+let rec repeats num digits times =
+  if times = 1 then num
+  else num + (Int.pow 10 digits) * repeats num digits (times - 1)
+
+let%test_unit "repeats digits" =
+  [%test_eq:int] (repeats 123 3 3) 123123123
+
 let filter_range range acc num =
   if num >= (fst range) && num <= (snd range)
     then num :: acc
   else acc
 
+let lower_str range = sprintf "%d" (fst range)
+let upper_str range = sprintf "%d" (snd range)
+
 let find_ids range : int list = 
-  let lower = sprintf "%d" (fst range) in
-  let upper = sprintf "%d" (snd range) in
+  let lower = lower_str range in
+  let upper = upper_str range in
   let num_chars = (String.length upper / 2) * 2 in
   let lower_trunc =
     if String.length lower < num_chars
@@ -50,7 +60,43 @@ let%test_unit "find ids with 9 and 22" =
 let%test_unit "find ids with 77 and 101" =
   [%test_eq: int list] (find_ids (77, 101)) [99; 88; 77]
 
+let rec get_next_ids range num num_repeats =
+  let repeat_len = String.length (sprintf "%d" num) in
+  let repeated = repeats num repeat_len num_repeats in
+  if repeated > snd range then []
+  else let (next_num, next_repeats) =
+    if num + 1 = (Int.pow 10 repeat_len) then ((num + 1) / 10, num_repeats + 1)
+    (* roll over to next repeat *)
+    else (num + 1, num_repeats) in
+  if repeated < fst range || num_repeats = 1 then get_next_ids range next_num next_repeats
+  else repeated :: get_next_ids range next_num next_repeats
+
+let ids_by_length range length =
+  (* repeat is given as tuple (first digits, length) *)
+  let lower = lower_str range in
+  let first_digits_init = Int.of_string (String.prefix lower length) in
+  let num_repeats_init = (String.length lower) / length in
+  (* number of repeats to at most match length of lower string *)
+  get_next_ids range first_digits_init num_repeats_init
+
+let find_all_repeats range : int list =
+  let repeat_lengths = List.range 1 (String.length (upper_str range) / 2 + 1) in
+  (* for each possible repeat length, enumerate all repeats in range: *)
+  let all_repeats = List.fold_left repeat_lengths ~init:[] ~f:(fun acc a -> List.append (ids_by_length range a) acc) in
+  List.stable_dedup ~compare:(-) all_repeats
+
+let%test_unit "find any size repeats with 11 and 22" =
+  [%test_eq: int list] (find_all_repeats (11, 22)) [11; 22]
+let%test_unit "find any size repeats with 9 and 22" =
+  [%test_eq: int list] (find_all_repeats (9, 22)) [11; 22]
+let%test_unit "find any size repeats with 111 and 112" =
+  [%test_eq: int list] (find_all_repeats (111, 112)) [111]
+let%test_unit "find any size repeats with 998 and 1111" =
+  [%test_eq: int list] (find_all_repeats (998, 1111)) [1010; 1111; 999]
+
 let run filename =
   let input = get_input filename in
   let invalid_ids = List.map input ~f:(fun a -> List.fold_left (find_ids a) ~init:0 ~f:(+)) in
-  printf "part 1: %d\n" (List.fold_left invalid_ids ~init:0 ~f:(+))
+  printf "part 1: %d\n" (List.fold_left invalid_ids ~init:0 ~f:(+));
+  let invalid_ids_anysize = List.map input ~f:(fun a -> List.fold_left (find_all_repeats a) ~init:0 ~f:(+)) in
+  printf "part 2: %d\n" (List.fold_left invalid_ids_anysize ~init:0 ~f:(+))
